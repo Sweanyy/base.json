cf9a9b66a82916f44152d9caab7765e6879e0743
Sweanyy <52612411+Sweanyy@users.noreply.github.com>
2021-03-29 01:00:17 +0200

    Add files via upload

---
+++ typings/index.d.ts
@@ -0,0 +1,17 @@
+declare module "base.json" {
+    class DataBase {
+        file: string;
+
+        public constructor(file: string);
+
+        public add(name: string, data: string, value: number): void;
+        public remove(name: string, data: string, value: number): void;
+        public divide(name: string, data: string, value: number): void;
+        public multiply(name: string, data: string, value: number): void;
+        public set(name: string, data: string, value: any): void;
+        public get(name: string, data: string): any;
+        public push(name: string, data: string, value: any): void;
+        public delete(name?: string, data?: string): void;
+        public save(): void;
+    }
+}
