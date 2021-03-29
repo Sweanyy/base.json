426c3e7a9da765bf2a5eaf2d929a68fd5bbef945
Sweanyy <52612411+Sweanyy@users.noreply.github.com>
2021-03-29 02:29:59 +0200

    Update push.js

--- examples/push.js
+++ examples/push.js
@@ -1,3 +1,11 @@
 const { DataBase } = require('base.json');
 
 const db = new DataBase('examples/database/push.json');
+
+db.push('push', 'array', 'Hello');
+// { "push": { "array": ["Hello"] } }
+
+db.push('push', 'array', 'World');
+// { "push": { "array": ["Hello", "World"] } } 
+
+console.log(db.get('push', 'array').join(' '));
